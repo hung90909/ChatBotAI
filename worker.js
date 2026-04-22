c0f2da5e2d17eae1453681ea2c35bece8dede14c
hung90909 <114125660+hung90909@users.noreply.github.com>
2026-04-22 18:02:35 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -24,7 +24,8 @@ export default {
     }));
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyA6nUxKqYTACjA8sS57M7GwH-Lmx_loX1s`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyA6nUxKqYTACjA8sS57M7GwH-Lmx_loX1s`
+       ,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
